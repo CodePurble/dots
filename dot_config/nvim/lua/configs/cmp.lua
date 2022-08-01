d53d7964dce64ba26c1703bc58e37c162d9aa6c5
Ramprakash C <49452779+CodePurble@users.noreply.github.com>
2022-08-01 15:44:02 +0530

    config(nvim): remove luasnip checks from cmp mappings

--- dot_config/nvim/lua/configs/cmp.lua
+++ dot_config/nvim/lua/configs/cmp.lua
@@ -58,8 +58,6 @@ local options = {
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif require("luasnip").expand_or_jumpable() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
             else
                 fallback()
             end
@@ -70,8 +68,6 @@ local options = {
     ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
             cmp.select_prev_item()
-        elseif require("luasnip").jumpable(-1) then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
         else
             fallback()
         end
