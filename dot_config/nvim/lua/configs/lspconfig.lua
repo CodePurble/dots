726fed0441e86b52285aadabde8e1bfa3845dd3c
Ramprakash C <49452779+CodePurble@users.noreply.github.com>
2022-10-24 16:24:17 +0530

    config(nvim): `mason.nvim` config into own module

---
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,101 @@
+local lspconfig = require("lspconfig")
+-- ref: https://github.com/hrsh7th/nvim-cmp#recommended-configuration
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    -- Mappings.
+    local opts = { noremap = true, silent = true }
+    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    buf_set_keymap("n", "<space>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+end
+
+-- local runtime_path = vim.split(package.path, ';')
+-- table.insert(runtime_path, "lua/?.lua")
+-- table.insert(runtime_path, "lua/?/init.lua")
+lspconfig.sumneko_lua.setup({
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+lspconfig.pyright.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+lspconfig.clangd.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+lspconfig.rust_analyzer.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+lspconfig.texlab.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+lspconfig.marksman.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+-- WARN: INSTALL MANUALLY FROM AUR
+-- A generic Linux binary for is not provided, so nvim-lsp-installer cannot get
+-- it from anywhere yet
+-- lspconfig.verible.setup{
+--     on_attach = on_attach,
+--     capabilities = capabilities
+-- }
+
+require("clangd_extensions").setup({
+    server = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    },
+})
+
+lspconfig.svls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
---
+++ dot_config/nvim/lua/configs/mason.lua
@@ -0,0 +1,45 @@
+require("mason").setup({
+    ui = {
+        icons = {
+            -- The list icon to use for installed packages.
+            package_installed = "",
+            -- The list icon to use for packages that are pending installation.
+            package_pending = "",
+            -- The list icon to use for packages that are not installed.
+            package_uninstalled = "",
+        },
+        keymaps = {
+            -- Keymap to expand a package in the UI
+            toggle_package_expand = "<CR>",
+            -- Keymap to install the package under the current cursor position
+            install_package = "i",
+            -- Keymap to reinstall/update the package under the current cursor position
+            update_package = "u",
+            -- Keymap to check for new version for the package under the current cursor position
+            check_package_version = "c",
+            -- Keymap to update all installed packages
+            update_all_packages = "U",
+            -- Keymap to check which installed packages are outdated
+            check_outdated_packages = "C",
+            -- Keymap to uninstall a package
+            uninstall_package = "X",
+        },
+    },
+
+    -- Controls to which degree logs are written to the log file. It's useful
+    -- to set this to vim.log.levels.DEBUG when debugging issues with package
+    -- installations.
+    log_level = vim.log.levels.INFO,
+})
+
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "clangd",
+        "sumneko_lua",
+        "pyright",
+        "texlab",
+        "marksman",
+    },
+    automatic_installation = false,
+})
+
