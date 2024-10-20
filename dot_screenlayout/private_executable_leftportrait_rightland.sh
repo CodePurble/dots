43164931eb537f942359a4ccefcbd0e7f7fffb7c
Ramprakash C <49452779+CodePurble@users.noreply.github.com>
2024-10-20 14:47:00 +0530

    config(xrandr): add multimon set script

---
+++ dot_screenlayout/private_executable_leftportrait_rightland.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x0 --rotate left --output DP-3 --off --output HDMI-0 --primary --mode 1920x1080 --pos 1080x581 --rotate normal --output DP-4 --off --output DP-5 --off
